735ad4b6823c71e4e158fad6a4219b9e8a476695
Igor_Polyakov <itworldigpo@gmail.com>
2023-10-04 14:12:33 +0300

    app js

--- app.js
+++ app.js
@@ -1,74 +1,74 @@
 //  ---------------------BOY TEXT-----------------------------------
 
 
-// const boyText = document.querySelector ('.main__boy-text');
+const boyText = document.querySelector ('.main__boy-text');
 
 
-// let yourName = prompt ('Привет! Как тебя зовут?');
+let yourName = prompt ('Привет! Как тебя зовут?');
 
 
-// function showName() {
-//     boyText.innerHTML = `Привет,  ${yourName} !!`
-// };
-// setTimeout (showName, 500);
+function showName() {
+    boyText.innerHTML = `Привет,  ${yourName} !!`
+};
+setTimeout (showName, 500);
 
-// setTimeout (showName1, 1900);
+setTimeout (showName1, 1900);
 
-// function showName1() {
-//     boyText.innerHTML = (`Меня зовут Ку-Ку.`)
-// };
+function showName1() {
+    boyText.innerHTML = (`Меня зовут Ку-Ку.`)
+};
 
-// setTimeout (showName2, 5500);
-// function showName2() {
-//     boyText.innerHTML = (`Давай поиграем`)
-// };
+setTimeout (showName2, 5500);
+function showName2() {
+    boyText.innerHTML = (`Давай поиграем`)
+};
 
-// setTimeout (showName3, 8500);
-// function showName3() {
-//     boyText.innerHTML = (`Эта игра для тебя`)
-// };
+setTimeout (showName3, 8500);
+function showName3() {
+    boyText.innerHTML = (`Эта игра для тебя`)
+};
 
-// setTimeout (showName4, 11000);
-// function showName4() {
-//     boyText.innerHTML = (`Помести круг в круг`)
-// };
+setTimeout (showName4, 11000);
+function showName4() {
+    boyText.innerHTML = (`Помести круг в круг`)
+};
 
 
 //  -------------------------------------------------------------------
 
 //  ---------------------BOY MOVE-------------------------------------
 
-// function boyMove(){
-//     let elem = document.querySelector ('.move-img1');
-//     let pos = 0;
-//     let int = setInterval(frame, 140)
-//     function frame (){
-//         if (pos == 30){
-//             clearInterval (int);
-//             elem.classList.remove('move__boy-first')
-//         } else {
-//             pos++;
-//             elem.classList.toggle ('move__boy-first')
-//         }
-//     }
-// };
-// boyMove();
+function boyMove(){
+    let elem = document.querySelector ('.move-img1');
+    let pos = 0;
+    let int = setInterval(frame, 140)
+    function frame (){
+        if (pos == 30){
+            clearInterval (int);
+            elem.classList.remove('move__boy-first')
+        } else {
+            pos++;
+            elem.classList.toggle ('move__boy-first')
+        }
+    }
+};
+boyMove();
 
-// function boyMoveSecond(){
-//     let elem = document.querySelector ('.move-img4');
-//     let pos = 0;
-//     let int = setInterval(frame, 140)
-//     function frame (){
-//         if (pos == 30){
-//             clearInterval (int);
-//             elem.classList.remove('move__boy-second')
-//         } else {
-//             pos++;
-//             elem.classList.toggle ('move__boy-second')
-//         }
-//     }
-// };
-// boyMoveSecond();
+function boyMoveSecond(){
+    let elem = document.querySelector ('.move-img4');
+    let pos = 0;
+    let int = setInterval(frame, 140)
+    function frame (){
+        if (pos == 30){
+            clearInterval (int);
+            elem.classList.remove('move__boy-second')
+        } else {
+            pos++;
+            elem.classList.toggle ('move__boy-second')
+        }
+    }
+};
+boyMoveSecond();
 //  -------------------------------------------------------------------
 
 //  -----------------------------Field--------------------------------------
@@ -81,8 +81,6 @@
 // } );
 
 
-
-//  -------------------------------------------------------------------
 const gragField = document.querySelector('.main__field');
 const gragItem = document.querySelector('.main__ball');
 
@@ -173,3 +171,5 @@ gragItem.addEventListener("mousedown", function (event) {
 gragItem.addEventListener("dragstart", function (event) {
 	event.preventDefault();
 });
+
+//  -------------------------------------------------------------------
